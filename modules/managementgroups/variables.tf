da39e911a2406fd041f031af99618202a49fd047
mohit <mohit@upendrasingh2020outlook.onmicrosoft.com>
2025-06-09 15:12:07 +0530

    Fix display name for Terraform apply step in management groups module

--- modules/managementgroups/variables.tf
+++ modules/managementgroups/variables.tf
@@ -26,12 +26,6 @@ variable "location" {
   default     = "eastus"
 }
 
-variable "admin_password" {
-  description = "The admin password"
-  type        = string
-  sensitive   = true
-}
-
 variable "prefix" {
   description = "Prefix for all resources"
   type        = string
