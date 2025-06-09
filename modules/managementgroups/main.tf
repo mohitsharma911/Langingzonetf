352cffa8d7535174fa3fd0d02e17db562fcae396
mohit <mohit@upendrasingh2020outlook.onmicrosoft.com>
2025-06-09 13:59:27 +0530

    Refactor management group resources to use static names and improve hierarchy

--- modules/managementgroups/main.tf
+++ modules/managementgroups/main.tf
@@ -1,28 +1,40 @@
-resource "azurerm_management_group" "root" {
-  name        = var.root_management_group_name
-  display_name = var.root_management_group_display_name
+resource "azurerm_management_group" "tenant_root" {
+  name        = "TenantRoot"
+  display_name = "Tenant Root Group"
 }
 
 resource "azurerm_management_group" "platform" {
-  name        = var.platform_management_group_name
-  display_name = var.platform_management_group_display_name
-  parent_management_group_id = azurerm_management_group.root.id
+  name        = "Platform"
+  display_name = "Platform Management Group"
+  parent_management_group_id = azurerm_management_group.tenant_root.id
 }
 
 resource "azurerm_management_group" "environments" {
-  name        = var.environments_management_group_name
-  display_name = var.environments_management_group_display_name
+  name        = "Environments"
+  display_name = "Environments Management Group"
   parent_management_group_id = azurerm_management_group.platform.id
 }
 
 resource "azurerm_management_group" "sandbox" {
-  name        = var.sandbox_management_group_name
-  display_name = var.sandbox_management_group_display_name
-  parent_management_group_id = azurerm_management_group.environments.id
+  name        = "Sandbox"
+  display_name = "Sandbox Management Group"
+  parent_management_group_id = azurerm_management_group.platform.id
+}
+
+resource "azurerm_management_group" "non_prod" {
+  name        = "NonProd"
+  display_name = "Non-Production Management Group"
+  parent_management_group_id = azurerm_management_group.platform.id
+}
+
+resource "azurerm_management_group" "production" {
+  name        = "Production"
+  display_name = "Production Management Group"
+  parent_management_group_id = azurerm_management_group.platform.id
 }
 
 resource "azurerm_management_group" "decommissioned" {
-  name        = var.decommissioned_management_group_name
-  display_name = var.decommissioned_management_group_display_name
-  parent_management_group_id = azurerm_management_group.environments.id
+  name        = "Decommissioned"
+  display_name = "Decommissioned Management Group"
+  parent_management_group_id = azurerm_management_group.platform.id
 }
