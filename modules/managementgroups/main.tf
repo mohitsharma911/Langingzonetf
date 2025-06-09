a064f5de7f0e2c38e01aeb8594f4ce2fb4cf8d17
mohit <mohit@upendrasingh2020outlook.onmicrosoft.com>
2025-06-09 17:31:22 +0530

    Update display names for management groups and adjust parent IDs

--- modules/managementgroups/main.tf
+++ modules/managementgroups/main.tf
@@ -14,26 +14,56 @@ provider "azurerm" {
 
 resource "azurerm_management_group" "IMS_root" {
   name        = "IMSRoot"
-  display_name = "IMS Root Management Group"
+  display_name = "IMS Root"
   parent_management_group_id = var.root_management_group_id # Replace with your actual root management group ID  
 }
 
 resource "azurerm_management_group" "platform" {
   name        = "Platform"
-  display_name = "Platform Management Group"
+  display_name = "Platform"
   parent_management_group_id = azurerm_management_group.IMS_root.id
 }
 
 resource "azurerm_management_group" "environments" {
   name        = "Environments"
-  display_name = "Environments Management Group"
-  parent_management_group_id = azurerm_management_group.platform.id
+  display_name = "Environments"
+  parent_management_group_id = azurerm_management_group.IMS_root.id
+}
+
+resource "azurerm_management_group" "Development" {
+  name        = "Development"
+  display_name = "Development"
+  parent_management_group_id = azurerm_management_group.environments.id
+}
+
+resource "azurerm_management_group" "Test" {
+  name        = "Test"
+  display_name = "Test"
+  parent_management_group_id = azurerm_management_group.environments.id
+}
+
+resource "azurerm_management_group" "PreProduction" {
+  name        = "PreProduction"
+  display_name = "PreProduction"
+  parent_management_group_id = azurerm_management_group.environments.id
+}
+
+resource "azurerm_management_group" "envproduction" {
+  name        = "Production"
+  display_name = "Production"
+  parent_management_group_id = azurerm_management_group.environments.id
 }
 
 resource "azurerm_management_group" "sandbox" {
   name        = "Sandbox"
-  display_name = "Sandbox Management Group"
-  parent_management_group_id = azurerm_management_group.platform.id
+  display_name = "Sandbox"
+  parent_management_group_id = azurerm_management_group.IMS_root.id
+}
+
+resource "azurerm_management_group" "decommissioned" {
+  name        = "Decommissioned"
+  display_name = "Decommissioned"
+  parent_management_group_id = azurerm_management_group.IMS_root.id
 }
 
 resource "azurerm_management_group" "non_prod" {
@@ -48,8 +78,3 @@ resource "azurerm_management_group" "production" {
   parent_management_group_id = azurerm_management_group.platform.id
 }
 
-resource "azurerm_management_group" "decommissioned" {
-  name        = "Decommissioned"
-  display_name = "Decommissioned Management Group"
-  parent_management_group_id = azurerm_management_group.platform.id
-}
