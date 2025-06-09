17debe7a86f904529a10e6b0f4f7ce1eef1fdfc3
mohit <mohit@upendrasingh2020outlook.onmicrosoft.com>
2025-06-09 14:27:10 +0530

    Remove management group resources and related outputs to streamline configuration

--- outputs.tf
+++
@@ -1,8 +0,0 @@
-output "management_group_ids" {
-  value = {
-    platform_management_group_id = module.management_groups.platform_management_group_id
-    landing_zone_management_group_id = module.management_groups.landing_zone_management_group_id
-    sandbox_management_group_id = module.management_groups.sandbox_management_group_id
-    decommissioned_management_group_id = module.management_groups.decommissioned_management_group_id
-  }
-}
