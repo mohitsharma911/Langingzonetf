3a25dbff7de685f5b22507cc8c456388782ed44e
mohit <mohit@upendrasingh2020outlook.onmicrosoft.com>
2025-06-09 14:30:48 +0530

    Remove unused top-level management group variables and add backend configuration variables for improved clarity and structure

---
+++ modules/managementgroups/variables copy.tf
@@ -0,0 +1,94 @@
+
+variable "platform_mg_defaults_enable" {
+  description = "Deploys Management, Identity and Connectivity Management Groups beneath Platform Management Group if set to true."
+  type        = bool
+  default     = true
+}
+
+variable "landing_zone_mg_children" {
+  description = "Dictionary Object to allow additional or different child Management Groups of IMS Root Management Group to be deployed."
+  type        = map(any)
+  default     = {}
+}
+
+variable "platform_mg_children" {
+  description = "Dictionary Object to allow additional or different child Management Groups of Platform Management Group to be deployed."
+  type        = map(any)
+  default     = {}
+}
+
+variable "telemetry_opt_out" {
+  description = "Set Parameter to true to Opt-out of deployment telemetry."
+  type        = bool
+  default     = false
+}
+
+variable "root_management_group_name" {
+  description = "The name of the root management group."
+  type        = string
+}
+
+variable "root_management_group_display_name" {
+  description = "The display name of the root management group."
+  type        = string
+}
+
+variable "platform_management_group_name" {
+  description = "The name of the platform management group."
+  type        = string
+}
+
+variable "platform_management_group_display_name" {
+  description = "The display name of the platform management group."
+  type        = string
+}
+
+variable "environments_management_group_name" {
+  description = "The name of the environments management group."
+  type        = string
+}
+
+variable "environments_management_group_display_name" {
+  description = "The display name of the environments management group."
+  type        = string
+  }
+
+variable "sandbox_management_group_name" {
+  description = "The name of the sandbox management group."
+  type        = string
+}
+
+variable "sandbox_management_group_display_name" {
+  description = "The display name of the sandbox management group."
+  type        = string
+  }
+
+variable "decommissioned_management_group_name" {
+  description = "The name of the decommissioned management group."
+  type        = string
+}
+
+variable "decommissioned_management_group_display_name" {
+  description = "The display name of the decommissioned management group."
+  type        = string
+}
+
+variable "backend_resource_group_name" {
+  description = "The name of the backend resource group"
+  type        = string
+}
+
+variable "backend_storage_account_name" {
+  description = "The name of the backend storage account"
+  type        = string
+}
+
+variable "backend_container_name" {
+  description = "The name of the backend container"
+  type        = string
+}
+
+variable "backend_key" {
+  description = "The key for the backend state file"
+  type        = string
+}
--- modules/managementgroups/variables.tf
+++ modules/managementgroups/variables.tf
@@ -1,30 +1,3 @@
-variable "top_level_management_group_prefix" {
-  description = "Prefix used for the management group hierarchy."
-  type        = string
-}
-
-variable "top_level_management_group_suffix" {
-  description = "Optional suffix for the management group hierarchy."
-  type        = string
-}
-
-variable "top_level_management_group_display_name" {
-  description = "Display name for top level management group."
-  type        = string
-}
-
-variable "top_level_management_group_parent_id" {
-  description = "Optional parent for Management Group hierarchy."
-  type        = string
-  default     = ""
-}
-
-variable "landing_zone_mg_defaults_enable" {
-  description = "Deploys Corp & Online Management Groups beneath IMS Root Management Group if set to true."
-  type        = bool
-  default     = true
-}
-
 variable "platform_mg_defaults_enable" {
   description = "Deploys Management, Identity and Connectivity Management Groups beneath Platform Management Group if set to true."
   type        = bool
@@ -49,56 +22,6 @@ variable "telemetry_opt_out" {
   default     = false
 }
 
-variable "root_management_group_name" {
-  description = "The name of the root management group."
-  type        = string
-}
-
-variable "root_management_group_display_name" {
-  description = "The display name of the root management group."
-  type        = string
-}
-
-variable "platform_management_group_name" {
-  description = "The name of the platform management group."
-  type        = string
-}
-
-variable "platform_management_group_display_name" {
-  description = "The display name of the platform management group."
-  type        = string
-}
-
-variable "environments_management_group_name" {
-  description = "The name of the environments management group."
-  type        = string
-}
-
-variable "environments_management_group_display_name" {
-  description = "The display name of the environments management group."
-  type        = string
-  }
-
-variable "sandbox_management_group_name" {
-  description = "The name of the sandbox management group."
-  type        = string
-}
-
-variable "sandbox_management_group_display_name" {
-  description = "The display name of the sandbox management group."
-  type        = string
-  }
-
-variable "decommissioned_management_group_name" {
-  description = "The name of the decommissioned management group."
-  type        = string
-}
-
-variable "decommissioned_management_group_display_name" {
-  description = "The display name of the decommissioned management group."
-  type        = string
-}
-
 variable "backend_resource_group_name" {
   description = "The name of the backend resource group"
   type        = string
@@ -117,4 +40,90 @@ variable "backend_container_name" {
 variable "backend_key" {
   description = "The key for the backend state file"
   type        = string
+}
+
+# Management Group variables matching main.tf
+
+variable "tenant_root_management_group_name" {
+  description = "The name of the tenant root management group."
+  type        = string
+  default     = "TenantRoot"
+}
+
+variable "tenant_root_management_group_display_name" {
+  description = "The display name of the tenant root management group."
+  type        = string
+  default     = "Tenant Root Group"
+}
+
+variable "platform_management_group_name" {
+  description = "The name of the platform management group."
+  type        = string
+  default     = "Platform"
+}
+
+variable "platform_management_group_display_name" {
+  description = "The display name of the platform management group."
+  type        = string
+  default     = "Platform Management Group"
+}
+
+variable "environments_management_group_name" {
+  description = "The name of the environments management group."
+  type        = string
+  default     = "Environments"
+}
+
+variable "environments_management_group_display_name" {
+  description = "The display name of the environments management group."
+  type        = string
+  default     = "Environments Management Group"
+}
+
+variable "sandbox_management_group_name" {
+  description = "The name of the sandbox management group."
+  type        = string
+  default     = "Sandbox"
+}
+
+variable "sandbox_management_group_display_name" {
+  description = "The display name of the sandbox management group."
+  type        = string
+  default     = "Sandbox Management Group"
+}
+
+variable "non_prod_management_group_name" {
+  description = "The name of the non-production management group."
+  type        = string
+  default     = "NonProd"
+}
+
+variable "non_prod_management_group_display_name" {
+  description = "The display name of the non-production management group."
+  type        = string
+  default     = "Non-Production Management Group"
+}
+
+variable "production_management_group_name" {
+  description = "The name of the production management group."
+  type        = string
+  default     = "Production"
+}
+
+variable "production_management_group_display_name" {
+  description = "The display name of the production management group."
+  type        = string
+  default     = "Production Management Group"
+}
+
+variable "decommissioned_management_group_name" {
+  description = "The name of the decommissioned management group."
+  type        = string
+  default     = "Decommissioned"
+}
+
+variable "decommissioned_management_group_display_name" {
+  description = "The display name of the decommissioned management group."
+  type        = string
+  default     = "Decommissioned Management Group"
 }
