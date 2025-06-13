a33d065b58d38de4d03e50e3079fe11cc5bfe22e
mohit <mohit@upendrasingh2020outlook.onmicrosoft.com>
2025-06-13 16:17:53 +0530

    Refactor Terraform backend configuration to required providers section

--- modules/managementgroups/main.tf
+++ modules/managementgroups/main.tf
@@ -1,14 +1,12 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "${var.backend_resource_group_name}"
-    storage_account_name = "${var.backend_storage_account_name}"
-    container_name       = "${var.backend_container_name}"
-    key                  = "${var.backend_key}" # Path to the state file in the container
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0" # Adjust the version as needed
+    }
   }
-}
 
-provider "azurerm" {
-  features {}
+  required_version = ">= 1.0"
 }
 
 
