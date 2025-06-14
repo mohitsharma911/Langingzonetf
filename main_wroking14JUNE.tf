0c1a682825558046fe6147c146e07033989a6628
mohit <mohit@upendrasingh2020outlook.onmicrosoft.com>
2025-06-14 17:41:30 +0530

    Refactor Terraform management groups workflow and configuration, updating backend state file naming and removing obsolete files.

--- main_wroking14JUNE.tf
+++ main_wroking14JUNE.tf
@@ -6,6 +6,15 @@ terraform {
     }
   }
   required_version = ">= 1.0"
+
+  backend "azurerm" {
+    resource_group_name  = "${var.backend_resource_group_name}"
+    storage_account_name = "${var.backend_storage_account_name}"
+    container_name       = "${var.backend_container_name}"
+    key                  = "${var.backend_key}" # Path to the state file in the container
+    use_oidc_auth        = true
+    use_azuread_auth     = true
+  }
 }
 
 provider "azurerm" {
