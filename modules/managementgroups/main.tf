31880c13947e5390c30a27aad2d1c8063f71bb3b
mohit <mohit@upendrasingh2020outlook.onmicrosoft.com>
2025-06-16 13:55:14 +0530

    Refactor backend configuration to use variable syntax for improved readability

--- modules/managementgroups/main.tf
+++ modules/managementgroups/main.tf
@@ -8,10 +8,10 @@ terraform {
   required_version = ">= 1.0"
 
   backend "azurerm" {
-    resource_group_name  = "${var.backend_resource_group_name}"
-    storage_account_name = "${var.backend_storage_account_name}"
-    container_name       = "${var.backend_container_name}"
-    key                  = "${var.backend_key}" # Path to the state file in the container
+    resource_group_name  = var.backend_resource_group_name
+    storage_account_name = var.backend_storage_account_name
+    container_name       = var.backend_container_name
+    key                  = var.backend_key # Path to the state file in the container
     use_oidc_auth        = true
     use_azuread_auth     = true
   }
