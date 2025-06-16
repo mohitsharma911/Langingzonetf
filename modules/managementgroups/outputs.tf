52fa95e717965f7cb232b3ed8fb3335c404bed07
mohit <mohit@upendrasingh2020outlook.onmicrosoft.com>
2025-06-16 17:42:36 +0530

    Remove unused management group output for clarity and maintainability

--- modules/managementgroups/outputs.tf
+++ modules/managementgroups/outputs.tf
@@ -1,6 +1,5 @@
 output "management_group_ids" {
   value = {
-    ims_root_group_id    = azurerm_management_group.IMS_root.id
     platform_group_id     = azurerm_management_group.platform.id
     environments_group_id  = azurerm_management_group.environments.id
     sandbox_group_id      = azurerm_management_group.sandbox.id
